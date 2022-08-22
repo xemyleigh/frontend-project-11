f90b0872c45f45637530220eb04d1a9a30574000
alexander <emyleigh@inbox.ru>
2022-08-22 03:23:54 +0300

    add loaders

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,16 @@ export default {
         path: path.join(__dirname, 'dist')
     },
 
+    module: {
+        rules: [
+            { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+        {
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+        },
+        ],
+    },
+
     plugins: [
         new HtmlWebpackPlugin({
             hash: true,
