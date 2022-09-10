7e855f51fa6d169d2ee99f086108510a976d23b4
alexander <emyleigh@inbox.ru>
2022-09-10 03:42:48 +0300

    deleting path and entry from webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,8 @@
-import { fileURLToPath } from 'url';
-import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default {
   mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'dist'),
-  },
 
   plugins: [
     new HtmlWebpackPlugin({
